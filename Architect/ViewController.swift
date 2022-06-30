37486e8bbfa7aa5869c53ee66b3cbc7b97a67193
korobitsyn <iamkorobitsyn@icloud.com>
2022-06-30 13:31:57 +0300

    initial

---
+++ Architect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architect
+//
+//  Created by Александр Коробицын on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        "initial"
+    }
+
+
+}
+
